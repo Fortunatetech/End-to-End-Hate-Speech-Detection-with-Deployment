d1cc0e708b87e363c915f212441cb5f4fc351bce
Ayodele Ayodeji <ayodeleayodeji250@gmail.com>
2023-12-01 12:05:19 +0100

    first commit

---
+++ hate/entity/artifact_entity.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+# Data ingestion artifacts
+@dataclass
+class DataIngestionArtifacts:
+    imbalance_data_file_path: str
+    raw_data_file_path: str
+
+
+@dataclass
+class DataTransformationArtifacts:
+    transformed_data_path: str
+
+
+
+@dataclass
+class ModelTrainerArtifacts: 
+    trained_model_path:str
+    x_test_path: list
+    y_test_path: list
+
+@dataclass
+class ModelEvaluationArtifacts:
+    is_model_accepted: bool    
+
+
+@dataclass
+class ModelPusherArtifacts:
+    bucket_name: str
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Ayodele Ayodeji",
+    author_email="ayodeleayodeji@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
